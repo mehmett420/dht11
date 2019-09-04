623251776dbdea26624cb8f34c262eb0c0ddaab7
mehmet ozdemir <mehmetozdemir206@gmail.com>
2019-09-04 14:47:25 +0300

    Update temp_bash.py

--- temp_bash.py
+++ temp_bash.py
@@ -1,24 +1,35 @@
 import grovepi
 import time
 import smtplib
+import requests
 dht_sensor_port=4
 dht_sensor_type=0
 import time
+ip_request = requests.get('https://get.geojs.io/v1/ip.json')
+my_ip = ip_request.json()['ip']  #ip_request.json() => {ip: '127.0.0.1'}
+print(my_ip)
+geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
+geo_request = requests.get(geo_request_url)
+geo_data = geo_request.json()
+#print({'organization': geo_data['organization']})
+gps=geo_data['organization']
+org=gps.replace(" ", "_")
+
 data=open('data.txt','w')
 [ temp,hum ] = grovepi.dht(dht_sensor_port,dht_sensor_type)
 print ('Temp: '+ str(temp) + '*C' + '\tHumidity:' + ' %'+ str(hum) + ' ' + str(time.strftime("%s",time.gmtime())))
-data.write('temperature,location=yourlocation ' + 'temp='+ str(temp))
+data.write('temperature,location=' + org + ' temp='+ str(temp))
 data.write('\n')
-data.write('temperature,location=yourlocation ' + 'hum='+ str(hum))
+data.write('temperature,location=' + org + ' hum='+ str(hum))
 
 def mail(content):
     mail = smtplib.SMTP("smtp.gmail.com",587)
     mail.starttls()
-    mail.login(''youremail@gmail.com','password')
-    mail.sendmail("youremail@gmail.com","targetmail@gmail.com",content)
+    mail.login('gozenintern@gmail.com','iskenHub123**')
+    mail.sendmail("gozenintern@gmail.com","muhammetkrn19@gmail.com",content)
 
 
-if temp>=21.0:
+if temp>=25.0:
     content = 'Oda sicakligi ' + str(temp) + '*C' + ' Sicaklik yuksek!'
     mail(content)
 elif temp<=15:
