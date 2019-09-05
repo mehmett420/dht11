8f73c875a1e2e1c9b3bf596b9c3d95e7ae3dcc8c
mehmet ozdemir <mehmetozdemir206@gmail.com>
2019-09-05 09:47:33 +0300

    Update temp_bash.py

--- temp_bash.py
+++ temp_bash.py
@@ -1,23 +1,24 @@
-import grovepi
+import grovepi #grove pi tanitmak icin
 import time
 import smtplib
 import requests
 import time
+
+#Grove pi ye bagli olan port
 dht_sensor_port=4
 dht_sensor_type=0
 
-
+#Ip adresinden konum tespiti icin.
 ip_request = requests.get('https://get.geojs.io/v1/ip.json')
 my_ip = ip_request.json()['ip']  #ip_request.json() => {ip: '127.0.0.1'}
 print(my_ip)
 geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
 geo_request = requests.get(geo_request_url)
 geo_data = geo_request.json()
-#print({'organization': geo_data['organization']})
 gps=geo_data['organization']
 org=gps.replace(" ", "_")
 
-
+#Dosyaya veri yazdirmak icin.
 data=open('data.txt','w')
 [ temp,hum ] = grovepi.dht(dht_sensor_port,dht_sensor_type)
 print ('Temp: '+ str(temp) + '*C' + '\tHumidity:' + ' %'+ str(hum) + ' ' + str(time.strftime("%s",time.gmtime())))
@@ -25,12 +26,14 @@ data.write('temperature,location=' + org + ' temp='+ str(temp))
 data.write('\n')
 data.write('temperature,location=' + org + ' hum='+ str(hum))
 
+#Mail gondermek icin.
 def mail(content):
     mail = smtplib.SMTP("smtp.gmail.com",587)
     mail.starttls()
     mail.login('sourcemailgmail.com','parola')
     mail.sendmail("sourcemail@gmail.com","targetmail@gmail.com",content)
 
+#Dereceler degistirilebilir.
 if temp>=25.0:
     content = 'Oda sicakligi ' + str(temp) + '*C' + ' Sicaklik yuksek!'
     mail(content)
