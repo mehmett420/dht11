9dbc466744fc5d7f42fa9ad4787e1bbdfd1aae6d
mehmet ozdemir <mehmetozdemir206@gmail.com>
2019-09-05 10:10:01 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,6 +2,6 @@
 while true; do
     python3 bash.py #python scriptini calistirmak icin
 
-    curl -i -XPOST "http://yourip:8086/write?db=mydb" --data-binary @data.txt #curl ile veri tabanina gelen cerileri yazdirmak icin
+    curl -i -XPOST "http://yourip:8086/write?db=mydb" --data-binary @data.txt #curl ile veri tabanina gelen verileri yazdirmak icin
     sleep 5s
 done
